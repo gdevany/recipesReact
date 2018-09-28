1b28464c13b6baca802a078487ea2f25bad2e81b
Greg Devany <greg.devany@yahoo.com>
2018-09-28 17:08:20 -0500

    ShowFullSizeImage: adjust placement of X to close full size and remove logo on full size

--- src/components/ShowFullSizeImage.js
+++ src/components/ShowFullSizeImage.js
@@ -1,33 +1,33 @@
 import React from "react";
-import {Image} from "cloudinary-react";
+import { Image } from "cloudinary-react";
 import PropTypes from "prop-types";
-
 
 // SHOW IF: <ProjectInd /> calls it
 function ShowFullSizeImage(props) {
-  window.scroll(0,130);
+  window.scroll(0, 130);
   return (
     <div className="container">
       <Image
         cloudName={props.cloudName}
         publicId={props.image}
         className="projIndimg"
-        />
+      />
       <div className="row">
         <span
           className="topRight white biggest shadow"
           onClick={props.toggleShow}
-          style={{cursor: "pointer"}}
-          >X
+          style={{ cursor: "pointer" }}
+        >
+          X
         </span>
       </div>
-      <div className="row">
+      {/* <div className="row">
         <img
           src={require(`../pics/${props.logo}`)}
           className="miniLogo"
           alt="logo"
         />
-      </div>
+      </div> */}
     </div>
   );
 }
@@ -36,7 +36,7 @@ ShowFullSizeImage.propTypes = {
   cloudName: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   toggleShow: PropTypes.func.isRequired,
-  logo: PropTypes.string.isRequired,
+  logo: PropTypes.string.isRequired
 };
 
 export default ShowFullSizeImage;
