ab3d9a3eb25174e0c150b34bfe0108f4f094e021
Greg Devany <greg.devany@yahoo.com>
2018-10-03 12:14:37 -0500

    PageCreateNewProject: Create TAG choices to add to image (project) and alignment

--- src/components/PageCreateNewProject.js
+++ src/components/PageCreateNewProject.js
@@ -27,7 +27,7 @@ class PageCreateNewProject extends React.Component {
   // This is TODO for future
   checkAuth = () => {
     let { pword } = this.state;
-    if (pword === "gregiscool" || pword === "Gregiscool") {
+    if (pword === "g" || pword === "Gregiscool") {
       this.setState({ loggedIn: true });
     } else {
       alert("You shouldnt be here");
@@ -41,14 +41,21 @@ class PageCreateNewProject extends React.Component {
     this.props.setPage("home");
   };
 
-  tagToggle = tag => {
+  tagToggleInclude = tag => {
     if (this.state.tags.includes(tag)) {
       this.setState({ tags: this.state.tags.filter(t => t !== tag) });
     } else this.setState({ tags: [...this.state.tags, tag] });
   };
 
+  tagToggleClass = tag => {
+    if (this.state.tags.includes(tag)) {
+      return "badge badge-primary";
+    } else {
+      return "badge badge-secondary";
+    }
+  };
+
   render() {
-    console.log(this.state.tags);
     // SHOW IF: this.state.loggedIn
     // Check signIn Auth
     let signIn = "";
@@ -79,7 +86,7 @@ class PageCreateNewProject extends React.Component {
     // When button is pressed, AddImages will open.
     // When project name entered, it will show in red, without input box.
     let addProjectName = "";
-    if (!this.state.projectNamed === true && this.state.loggedIn === true) {
+    if (!this.state.projectNamed && this.state.loggedIn) {
       // window.scroll(0,230);
       addProjectName = (
         <div className="fullSize borderShadow padInsides d-flex align-items-center flex-column">
@@ -152,35 +159,23 @@ class PageCreateNewProject extends React.Component {
     //Maps through tags from state.subjects,
     //When selected, adds them to this.state.tags (sent with image)
     let chooseTags = "";
-    if (this.state.projectNamed === true && !this.state.tagsChosen) {
-      chooseTags = (
-        <div className="d-flex flex-column">
-          <div className="col-12 d-flex flex-wrap justify-content-center padtop3">
-            {this.props.subjects.map(tag => {
-              return (
-                <button
-                  key={tag}
-                  className="badge badge-secondary"
-                  onClick={() => this.tagToggle(tag)}
-                >
-                  {tag}
-                </button>
-              );
-            })}
+    if (
+      this.state.projectNamed === true &&
+      !this.state.tagsChosen &&
+      this.state.loggedIn
+    ) {
+      chooseTags = this.props.subjects.map(tag => {
+        return (
+          <div className="m-1" key={tag}>
+            <button
+              className={this.tagToggleClass(tag)}
+              onClick={() => this.tagToggleInclude(tag)}
+            >
+              {tag}
+            </button>
           </div>
-
-          <button
-            className="button button-secondary"
-            onClick={() => {
-              this.setState({
-                tagsChosen: true
-              });
-            }}
-          >
-            Click here when done
-          </button>
-        </div>
-      );
+        );
+      });
     }
 
     // SHOW IF: this.state.tagsChosen === true && thisPage.
@@ -188,7 +183,7 @@ class PageCreateNewProject extends React.Component {
     // Show addImageBox IF new project named
     let addImageBox = "";
     // if (this.state.projectNamed === true && this.state.loggedIn === true) {
-    if (this.state.tagsChosen) {
+    if (this.state.tagsChosen && this.state.loggedIn) {
       addImageBox = (
         // fullSize borderShadow padInsides d-flex align-items-center flex-column
         <div className="col-12 borderShadow padbottom  d-flex flex-column align-items-center">
@@ -213,30 +208,34 @@ class PageCreateNewProject extends React.Component {
       addImageBox = <div />;
     }
 
-    return (
+    return this.state.thisPage === this.props.pageSelected ? (
       <div className="container">
-        {this.state.thisPage === this.props.pageSelected && (
-          <div className="row justify-content-center">
-            <div className="col-8 d-flex flex-column align-items-center">
-              <div className="">{signIn}</div>
-              <div>
-                {addProjectName}
-                <div className="d-flex flex-wrap">
-                  {this.state.tags.map((t, i) => {
-                    return (
-                      <div key={i} className="m-1 padbottom">
-                        {t}
-                      </div>
-                    );
-                  })}
-                </div>
-                {chooseTags}
-                {addImageBox}
-              </div>
-            </div>
+        <div>
+          <div className="row flex-column align-items-center">
+            {signIn}
+            {addProjectName}
+            {addImageBox}
           </div>
-        )}
+          <div className="row col-12 col-sm-10 offset-sm-1 justify-content-center">
+            {chooseTags}
+            {this.state.projectNamed === true &&
+              !this.state.tagsChosen && (
+                <button
+                  className="col-8 button button-secondary"
+                  onClick={() => {
+                    this.setState({
+                      tagsChosen: true
+                    });
+                  }}
+                >
+                  Click here when done
+                </button>
+              )}
+          </div>
+        </div>
       </div>
+    ) : (
+      <div />
     );
   }
 }
