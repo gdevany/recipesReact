79027f00df6fc420b02c47ade1552bc0fed492cd
Greg Devany <greg.devany@yahoo.com>
2019-06-12 18:28:12 -0500

    Begin: Rework Logo to be dynamic

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 function Logo(props) {
@@ -10,15 +10,22 @@ function Logo(props) {
   };
 
   let viewIt = "";
-    viewIt = (
-      <div className="row">
-        <div className="d-flex justify-content-center col-12 pad-top-resp">
-          <div className="seclogo">
-            <div style={logoImg} className="logoinit" />
-          </div>
-        </div>
+  viewIt = (
+    <div className="row justify-content-center">
+      <div className="d-flex pad-top-resp" style={{marginBottom: '10px'}}>
+        <div className="logo-box1 rounded text-center px-3 py-2">Devany</div>
+        <div className="logo-box2 rounded text-center px-3 pt-2">recipes</div>
       </div>
-    );
+    </div>
+
+    // <div className="row">
+    //   <div className="d-flex justify-content-center col-12 pad-top-resp">
+    //     <div className="seclogo">
+    //       <div style={logoImg} className="logoinit" />
+    //     </div>
+    //   </div>
+    // </div>
+  );
 
   return <div className="container-fluid logoBackground">{viewIt}</div>;
 }
@@ -31,7 +38,7 @@ Logo.propTypes = {
 function mapStateToProps(state) {
   return {
     logo: state.logo,
-    pageSelected: state.pageSelected,
+    pageSelected: state.pageSelected
   };
 }
 
