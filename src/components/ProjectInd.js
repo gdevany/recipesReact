f596532d8415b18b28ddbd2e3ea9acede2de1984
Greg Devany <greg.devany@yahoo.com>
2019-09-26 15:44:47 -0500

    projectInd: add back arrow to take back to home

--- src/components/ProjectInd.js
+++ src/components/ProjectInd.js
@@ -2,10 +2,12 @@ import React from "react";
 import axios from "axios";
 import { Image } from "cloudinary-react";
 import ShowFullSizeImage from "./ShowFullSizeImage";
-import {connect} from "react-redux";
-import {setProjectChosen} from "../actions";
+import { connect } from "react-redux";
+import { setProjectChosen, setPageSelect } from "../actions";
 import PropTypes from "prop-types";
 import { Animated } from "react-animated-css";
+
+var FontAwesome = require("react-fontawesome");
 
 // SHOW IF: (projectChosen)
 class ProjectInd extends React.Component {
@@ -105,16 +107,30 @@ class ProjectInd extends React.Component {
       <div className="container">
         <div className="row pt-2">
           <div className="projIndimg">{this.showFullSizeImage()}</div>
-            <div className="pt-5 col-12 col-md-6 d-flex text-left flex-column">
-              <div className="bigger projtitle text-truncate">
-                {this.props.projectChosen}
-              </div>
-              <div className="pt-3 mb-5">{this.showDescription()}</div>
+          <div className="pt-5 col-12 col-md-6 d-flex text-left flex-column">
+            <div
+              className="red"
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                this.props.setPage("home"), this.props.setProjectChosen("");
+              }}
+            >
+              <FontAwesome
+                className="mb-3 pr-3"
+                name="fas fa-arrow-left"
+                size="1x"
+              />
+              back
             </div>
-            <div className="col-12 offset-md-1 col-md-5 d-flex flex-column text-center align-items-center">
-              <small>Click image to toggle full size & quality</small>
-              {this.showProjInd()}
+            <div className="bigger projtitle text-truncate">
+              {this.props.projectChosen}
             </div>
+            <div className="pt-3 mb-5">{this.showDescription()}</div>
+          </div>
+          <div className="col-12 offset-md-1 col-md-5 d-flex flex-column text-center align-items-center">
+            <small>Click image to toggle full size & quality</small>
+            {this.showProjInd()}
+          </div>
         </div>
       </div>
     );
@@ -130,16 +146,22 @@ function mapStateToProps(state) {
   return {
     pageSelected: state.pageSelected,
     projectChosen: state.projectChosen,
-    cloudName: state.cloudName,
+    cloudName: state.cloudName
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    setProjectChosen: (proj) => {
+    setProjectChosen: proj => {
       dispatch(setProjectChosen(proj));
+    },
+    setPage: page => {
+      dispatch(setPageSelect(page));
     }
   };
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(ProjectInd);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProjectInd);
