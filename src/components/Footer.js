b5ffa5920b65bdfe8ce14e2d61ec2ccf1b6ac559
Greg Devany <greg.devany@yahoo.com>
2019-10-01 15:56:46 -0500

    Footer: add fafa github icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,34 @@
 import React from "react";
 
+var FontAwesome = require("react-fontawesome");
+
 function Footer() {
   // SHOW ALWAYS
   return (
     <div className="pb-4 text-center">
       <div className="mx-2 pt-5">Life is short. Eat good.</div>
-      <div className="fade-half">
-        <i
-          className="fa fa-facebook fa-2x mx-3 pt-3"
+      <div className="fade-half pt-3">
+        <FontAwesome
+          className="fab fa-facebook fa-2x mx-3"
           style={{ cursor: "pointer" }}
         />
-        <i
-          className="fa fa-twitter fa-2x mx-3 pt-3"
+        <FontAwesome
+          className="fab fa-twitter fa-2x mx-3"
           style={{ cursor: "pointer" }}
         />
-        <i
-          className="fa fa-instagram fa-2x mx-3 pt-3"
+        <FontAwesome
+          className="fab fa-instagram fa-2x mx-3"
           style={{ cursor: "pointer" }}
+        />
+        <FontAwesome
+          className="fab fa-github fa-2x mx-3"
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            window.open(
+              "https://github.com/gdevany/recipesReact",
+              "_blank"
+            );
+          }}
         />
       </div>
     </div>
